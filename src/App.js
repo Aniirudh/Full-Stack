6bc0094646f9e5485ee44d07a1ea2b55c0f4dfc6
Anirudh <anirudhudugani@gmail.com>
2022-04-13 12:54:10 +0530

    first comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const[userInput,setInput]=useState({
+    name:"",
+    usn:"",
+    age:"",
+    uni:"",
+    city:""
+  });
+  const[result,setResult]=useState("");
+  
+
+  const handleChange=(event)=>{
+    const obj={
+      name:event.target.value,
+      
+    }
+    setInput(userInput.name)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Name:<input onChange={handleChange} value={userInput}></input>
+
+      <button>{userInput}</button>
     </div>
   );
 }
